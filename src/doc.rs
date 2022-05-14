7c90f4c54c371b8ed7becf6fce733d3dee77648e
Nishant-Pall <palln6935@gmail.com>
2022-05-14 20:50:23 +0530

    Add generic lifetimes

--- src/doc.rs
+++ src/doc.rs
@@ -519,3 +519,51 @@
 //     // println!("{}", article.summarize_author());
 //     // println!("{}", tweet.summarize_author());
 // }
+
+// use std::fmt::Display;
+
+// struct Pair<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Pair<T> {
+//     fn new(x: T, y: T) -> Self {
+//         Self { x, y }
+//     }
+// }
+
+// impl<T: PartialOrd + Display> Pair<T> {
+//     fn cmp_display(&self) {
+//         if self.x >= self.y {
+//             println!("The largest number is: {}", self.x);
+//         } else {
+//             println!("The largest number is: {}", self.y);
+//         }
+//     }
+// }
+
+// fn main() {}
+
+// ---------------------------------------------------------------------------
+// GENERIC LIFETIMES
+// ---------------------------------------------------------------------------
+
+// fn main() {
+//     let string1 = String::from("abcd");
+//     let string2 = String::from("xyz");
+
+//     let result = longest(string1.as_str(), string2.as_str());
+//     println!("The longer string is: {}", result)
+// }
+
+// // &i32 -------> a reference
+// // &'a i32 ----> a reference with an explicit lifetime
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
