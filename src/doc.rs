69d245ded771fe74214342dbc96d9c1e7f3e9eed
Nishant-Pall <palln6935@gmail.com>
2022-05-14 19:48:02 +0530

    Add trait bounds

--- src/doc.rs
+++ src/doc.rs
@@ -431,3 +431,91 @@
 
 //     println!("p4.x is: {}, p4.y is: {}", p4.x, p4.y)
 // }
+
+// TRAITS
+// use std::fmt::{Debug, Display};
+
+// pub struct NewsArticle {
+//     pub headline: String,
+//     pub author: String,
+//     pub content: String,
+// }
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{} by {}", self.headline, self.author)
+//     }
+//     fn summarize_author(&self) -> String {
+//         format!("{}", self.author)
+//     }
+// }
+
+// pub struct Tweet {
+//     pub content: String,
+//     pub username: String,
+//     pub reply: bool,
+//     pub retweet: bool,
+// }
+
+// impl Summary for Tweet {
+//     fn summarize(&self) -> String {
+//         format!("{}: {}", self.username, self.content)
+//     }
+//     fn summarize_author(&self) -> String {
+//         format!("{}", self.username)
+//     }
+// }
+
+// pub trait Summary {
+//     fn summarize_author(&self) -> String;
+//     fn summarize(&self) -> String {
+//         // default implementation
+//         String::from("(Read more...)")
+//     }
+// }
+
+// // pub fn notify(item: &impl Summary) {
+// //     println!("Breaking news!: {}", item.summarize())
+// // }
+
+// // pub fn notify<T: Summary>(item: &T) {
+// //     println!("Breaking news!: {}", item.summarize())
+// // }
+
+// // pub fn notify(item1: &(impl Summary + Display), item2: &impl Summary) {
+// //     // ...
+// // }
+
+// // pub fn notify<T: Summary + Display>(item1: &T, item2: &T) {
+// //     //...
+// // }
+
+// pub fn some_function<T, U>(t: &T, u: &U)
+// where
+//     T: Display + Clone,
+//     U: Clone + Debug,
+// {
+//     // ...
+// }
+
+// fn main() {
+//     let tweet = Tweet {
+//         content: String::from("Hello World"),
+//         username: String::from("@johndoe"),
+//         reply: false,
+//         retweet: false,
+//     };
+
+//     let article = NewsArticle {
+//         headline: String::from("The sky is falling"),
+//         content: String::from("Yes, the sky is falling"),
+//         author: String::from("John Doe"),
+//     };
+
+//     // notify(&article);
+
+//     // println!("Tweet Summary: {}", tweet.summarize());
+//     // println!("News Summary: {}", article.summarize());
+//     // println!("{}", article.summarize_author());
+//     // println!("{}", tweet.summarize_author());
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,24 @@
-pub struct NewsArticle {
-    pub headline: String,
-    pub author: String,
-    pub content: String,
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
 }
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{} by {}", self.headline, self.author)
-    }
-    fn summarize_author(&self) -> String {
-        format!("{}", self.author)
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
     }
 }
 
-pub struct Tweet {
-    pub content: String,
-    pub username: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-    fn summarize_author(&self) -> String {
-        format!("{}", self.username)
+impl<T: PartialOrd + Display> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest number is: {}", self.x);
+        } else {
+            println!("The largest number is: {}", self.y);
+        }
     }
 }
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
-    fn summarize(&self) -> String {
-        // default implementation
-        String::from("(Read more...)")
-    }
-}
-
-fn main() {
-    let tweet = Tweet {
-        content: String::from("Hello World"),
-        username: String::from("@johndoe"),
-        reply: false,
-        retweet: false,
-    };
-
-    let article = NewsArticle {
-        headline: String::from("The sky is falling"),
-        content: String::from("Yes, the sky is falling"),
-        author: String::from("John Doe"),
-    };
-
-    println!("Tweet Summary: {}", tweet.summarize());
-    println!("News Summary: {}", article.summarize());
-    println!("{}", article.summarize_author());
-    println!("{}", tweet.summarize_author());
-}
+fn main() {}
