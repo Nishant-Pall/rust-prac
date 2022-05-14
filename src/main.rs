f90dcc8d3a742c726fe20b880125461fe55ca665
Nishant-Pall <palln6935@gmail.com>
2022-05-14 16:00:54 +0530

    Add loops

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,39 @@ fn main() {
     // let not_found = error_codes[1];
     let sum = my_func(10, 20);
     println!("{}", sum);
+
+    // Control flow
+    // one way is if, else if, else
+
+    // let condition = true;
+    // let number = if condition { 5 } else { 6 };
+
+    // loops
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter;
+        }
+        println!("{}", counter);
+    };
+    println!("result is: {}", result);
+
+    while counter != 0 {
+        counter -= 1;
+        println!("{}", counter);
+    }
+    println!("LIFT OFF");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("{}", element)
+    }
+
+    for number in 1..10 {
+        println!("{}", number)
+    }
 }
 
 fn my_func(x: i32, y: i32) -> i32 {
