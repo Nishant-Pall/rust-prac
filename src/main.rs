f610e426e46487c3439035e8d622ebee7c3e6426
Nishant-Pall <palln6935@gmail.com>
2022-05-14 16:00:54 +0530

    Add variables, functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // // this is valid, and is called variable shadowing
+    // let x = 4;
+    // let x = "six";
+
+    // // cant use const for return statements of functions
+    // const SUB_COUNT: u32 = 100_000;
+
+    // // tuple
+    // let tup = ("yo yo", 100);
+
+    // let (text, number) = tup;
+
+    // let text = tup.0;
+    // let number = tup.1;
+
+    // let error_codes = [400, 404, 500];
+    // let not_found = error_codes[1];
+    my_func();
+}
+
+fn my_func() {
+    println!("function called")
 }
