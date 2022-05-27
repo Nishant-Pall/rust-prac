2da56be277c1f16fe52260bd4c6c879e9c9cb0d7
Nishant-Pall <palln6935@gmail.com>
2022-05-27 16:53:16 +0530

    Add custom deref trait

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-use List::{Cons, Nil};
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 fn main() {
-    // let b = Box::new(5);
-    // println!("{}", b);
+    let x = 5;
+    let y = MyBox::new(x);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("rust"));
+
+    hello(&m);
+    // &MyBox<String> -> &String -> &str
+    // The automatic deref coercion feature of rust converts the type to the function signature,
+    // dereferencing &String gives us &str(string slice)
+
+    // If rust didnt have this feature we would have to do
+    hello(&(*m)[..])
+}
+
+fn hello(name: &str) {
+    println!("{}", name)
 }
