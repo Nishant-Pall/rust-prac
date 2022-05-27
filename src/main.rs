12742e45b1995a2de8f4771d305cd00741899267
Nishant-Pall <palln6935@gmail.com>
2022-05-27 13:21:59 +0530

    Add docs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
-struct Counter {
-    count: u32,
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = rust_prac::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
+fn main() {
+    println!("YOOOOOOOOOo");
 }
-
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
-
-#[test]
-fn calling_next_iterator() {
-    let mut counter = Counter::new();
-
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
-}
-
-fn main() {}
