46d1a52f68204cf2eaeee6159e9912936b91152f
Nishant-Pall <palln6935@gmail.com>
2022-05-27 13:30:07 +0530

    Add sub packages

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-/// Adds one to the number given.
-///
-/// # Examples
-///
-/// ```
-/// let arg = 5;
-/// let answer = rust_prac::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
-}
-
 fn main() {
     println!("YOOOOOOOOOo");
 }
