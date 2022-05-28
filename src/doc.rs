6483439342e2343efd065c3946e933bb2a8e6273
Nishant-Pall <palln6935@gmail.com>
2022-05-28 13:44:03 +0530

    Add reference counting

--- src/doc.rs
+++ src/doc.rs
@@ -921,3 +921,46 @@
 
 // ****************************************************************************************
 // ****************************************************************************************
+
+// use std::ops::Deref;
+
+// struct MyBox<T>(T);
+
+// impl<T> MyBox<T> {
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+
+//     fn deref(&self) -> &Self::Target {
+//         &self.0
+//     }
+// }
+
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+
+//     let m = MyBox::new(String::from("rust"));
+
+//     hello(&m);
+//     // &MyBox<String> -> &String -> &str
+//     // The automatic deref coercion feature of rust converts the type to the function signature,
+//     // dereferencing &String gives us &str(string slice)
+
+//     // If rust didnt have this feature we would have to do
+//     hello(&(*m)[..])
+// }
+
+// fn hello(name: &str) {
+//     println!("{}", name)
+// }
+
+// ****************************************************************************************
+// ****************************************************************************************
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
-use std::ops::Deref;
+use std::rc::Rc;
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
+use crate::List::{Cons, Nil};
 
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    println!("{}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("{}", Rc::strong_count(&a));
+    }
 
-    let m = MyBox::new(String::from("rust"));
-
-    hello(&m);
-    // &MyBox<String> -> &String -> &str
-    // The automatic deref coercion feature of rust converts the type to the function signature,
-    // dereferencing &String gives us &str(string slice)
-
-    // If rust didnt have this feature we would have to do
-    hello(&(*m)[..])
-}
-
-fn hello(name: &str) {
-    println!("{}", name)
+    println!("{}", Rc::strong_count(&a));
 }
