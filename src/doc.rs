f85d6f9501f596d9a62b58bd7793219efc1b030d
Nishant-Pall <palln6935@gmail.com>
2022-06-02 15:09:59 +0530

    Add state design

--- src/doc.rs
+++ src/doc.rs
@@ -1118,3 +1118,75 @@
 
 // ****************************************************************************************
 // ****************************************************************************************
+
+// use std::vec;
+
+// use rust_prac::{Button, Draw, Screen};
+
+// struct SelectBox {
+//     pub width: u32,
+//     pub height: u32,
+//     pub options: Vec<String>,
+// }
+
+// impl Draw for SelectBox {
+//     fn draw(&self) {
+//         // draw selectbox
+//     }
+// }
+
+// fn main() {
+//     let screen = Screen {
+//         components: vec![
+//             Box::new(SelectBox {
+//                 width: 100,
+//                 height: 100,
+//                 options: vec![
+//                     String::from("Yes"),
+//                     String::from("No"),
+//                     String::from("Maybe"),
+//                 ],
+//             }),
+//             Box::new(Button {
+//                 width: 100,
+//                 height: 100,
+//                 label: String::from("ok"),
+//             }),
+//         ],
+//     };
+
+//     screen.run();
+// }
+
+//  LIB CODE
+// pub trait Draw {
+//     fn draw(&self);
+// }
+
+// pub struct Screen {
+//     // dyn -> dynamic dispatch
+//     pub components: Vec<Box<dyn Draw>>,
+// }
+
+// impl Screen {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+// pub struct Button {
+//     pub width: u32,
+//     pub height: u32,
+//     pub label: String,
+// }
+
+// impl Draw for Button {
+//     fn draw(&self) {
+//         // draw button
+//     }
+// }
+
+// ****************************************************************************************
+// ****************************************************************************************
