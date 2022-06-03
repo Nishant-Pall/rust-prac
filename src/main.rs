08a93cb4cc6e92e757ff7403d74a404f470b5ebe
Nishant-Pall <palln6935@gmail.com>
2022-06-03 16:44:50 +0530

    Add unsafe rust scopes

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+fn main() {
+    let mut num = 5;
+
+    // raw pointers do not follow any ownership rules
+    // we cant dereference raw pointers unless we use
+    // unsafe keyword
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("{}", *r1);
+        println!("{}", *r2);
+    }
+
+    unsafe fn dangerous() {}
+
+    // unsafe functions can only be called inside unsafe blocks or functions
+    unsafe {
+        dangerous();
+    }
+}
