12df2c261815071442a1ad93c7eabf02335ab7ee
Nishant-Pall <palln6935@gmail.com>
2022-06-04 13:02:34 +0530

    Add advanced functions

--- src/doc.rs
+++ src/doc.rs
@@ -1190,3 +1190,54 @@
 
 // ****************************************************************************************
 // ****************************************************************************************
+
+// fn main() {
+//     let mut num = 5;
+
+//     // raw pointers do not follow any ownership rules
+//     // we cant dereference raw pointers unless we use
+//     // unsafe keyword
+//     let r1 = &num as *const i32;
+//     let r2 = &mut num as *mut i32;
+
+//     unsafe {
+//         println!("{}", *r1);
+//         println!("{}", *r2);
+//     }
+
+//     unsafe fn dangerous() {}
+
+//     // unsafe functions can only be called inside unsafe blocks or functions
+//     unsafe {
+//         dangerous();
+//     }
+// }
+
+// ****************************************************************************************
+// ****************************************************************************************
+
+// fn add_one(x: i32) -> i32 {
+//     x + 1
+// }
+
+// fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+//     f(arg) + f(arg)
+// }
+
+// fn do_twice_using_generics<T>(f: T, arg: i32) -> i32
+// where
+//     T: Fn(i32) -> i32,
+// {
+//     f(arg) + f(arg)
+// }
+
+// // Fn, FnMut, FnOnce
+
+// fn main() {
+//     let answer = do_twice(add_one, 5);
+
+//     println!("{}", answer)
+// }
+
+// ****************************************************************************************
+// ****************************************************************************************
